7dbce8f4e17a25190f5bb9da0f7cf6e5644d3aec
sumanta rout <sumantar@cybage.com>
2013-01-11 23:53:18 +0530

    Fixing Unit Testing Issues

---
+++ SampleTestLogicTest/SampleTestLogicTest.h
@@ -0,0 +1,13 @@
+//
+//  SampleTestLogicTest.h
+//  SampleTestLogicTest
+//
+//  Created by sumantar on 11/01/13.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SampleTestLogicTest : SenTestCase
+
+@end
---
+++ SampleTestLogicTest/SampleTestLogicTest.m
@@ -0,0 +1,32 @@
+//
+//  SampleTestLogicTest.m
+//  SampleTestLogicTest
+//
+//  Created by sumantar on 11/01/13.
+//
+//
+
+#import "SampleTestLogicTest.h"
+
+@implementation SampleTestLogicTest
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SampleTestLogicTest");
+}
+
+@end
